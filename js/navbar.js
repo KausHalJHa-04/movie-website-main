c8c01344c4d8fc4cbb88537e23e17255a0b56f05
Kaushal Kumar Jha <162674922+KausHalJHa-04@users.noreply.github.com>
2025-05-07 01:17:34 +0530

    Update navbar.js

--- js/navbar.js
+++ js/navbar.js
@@ -1,21 +1,10 @@
-// let hamburgerbtn = document.querySelector(".hamburger");
-// let nav_list = document.querySelector(".nav-list");
-// let closebtn = document.querySelector(".close");
-// hamburgerbtn.addEventListener("click", () => {
-//   nav_list.classList.add("active");
-// });
-// closebtn.addEventListener("click", () => {
-//   nav_list.classList.remove("active");
-// })
+let hamburgerbtn = document.querySelector(".hamburger");
+let nav_list = document.querySelector(".nav-list");
+let closebtn = document.querySelector(".close");
+hamburgerbtn.addEventListener("click", () => {
+  nav_list.classList.add("active");
+});
+closebtn.addEventListener("click", () => {
+  nav_list.classList.remove("active");
+})
 
-// filepath: c:\Users\kaushal jha\Downloads\movie-website-main\responsive-movie-website-main\filmlane-website-main\js\navbar.js
-// document.querySelectorAll('.nav-list a').forEach(anchor => {
-//   anchor.addEventListener('click', function (e) {
-//       e.preventDefault();
-//       const targetId = this.getAttribute('href').substring(1);
-//       const targetElement = document.getElementById(targetId);
-//       if (targetElement) {
-//           targetElement.scrollIntoView({ behavior: 'smooth' });
-//       }
-//   });
-// });
